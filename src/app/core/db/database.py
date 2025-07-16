3c129af77b963b11f593b31b5373ef6d647e226d
Oktavianus Matthew <113876986+oktavianusmatthew1010@users.noreply.github.com>
2025-07-16 10:16:44 +0700

    Update database.py

--- src/app/core/db/database.py
+++ src/app/core/db/database.py
@@ -15,7 +15,14 @@ DATABASE_URI = settings.POSTGRES_URI
 DATABASE_PREFIX = settings.POSTGRES_ASYNC_PREFIX
 DATABASE_URL = f"{DATABASE_PREFIX}{DATABASE_URI}"
 
-async_engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+async_engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+    connect_args={"timeout": 15},
+)
 
 local_session = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
 
